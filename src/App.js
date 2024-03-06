f50aa781d57b78ae77751ec7e4c186e16f423eea
CECurtiss <curtissclinton@gmail.com>
2024-03-05 19:15:01 -0600

    undo to error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Wine from "./pages/Wine";
 import Beer from "./pages/Beer";
 import Spirits from "./pages/Spirits";
 import Nav from "./components/Nav";
