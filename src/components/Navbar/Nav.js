97f9e0f0a432327b250340f8b9616c5a48cd5926
CECurtiss <curtissclinton@gmail.com>
2024-03-07 19:24:44 -0600

    replaced Link w/ NavLink in nav.js

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Nav, Navbar, Dropdown, NavDropdown } from "react-bootstrap";
 
 import corkbarrellogo from "../../images/corkbarrellogo.webp";
 import "./Nav.css";
 
-const NavigationBar = ()  => {
-  
-  const imgStyle = { height: "100px", width: "375px", boxShadow: "10px 5px 5px black", margin: "10px" };
-  
+const NavigationBar = () => {
+  const imgStyle = {
+    height: "100px",
+    width: "375px",
+    boxShadow: "10px 5px 5px black",
+    margin: "10px",
+  };
+
   const navDropStyle = {
     textDecoration: "none",
     fontFamily: "georgia",
@@ -16,48 +20,47 @@ const NavigationBar = ()  => {
     margin: "10px",
     color: "#054581",
     textShadow: "1px 1px 2px white",
-
   };
 
   return (
-    
     <Navbar className="navbarStyle">
       <Nav className="menuItemStyle">
         <div>
-          <Link to="/Wine" className="linkStyle">
-            <p className="menuItem">Wine</p></Link>
+          <NavLink to="/Wine" className="linkStyle">
+            <p className="menuItem">Wine</p>
+          </NavLink>
         </div>
       </Nav>
 
       <Nav className="menuItemStyle">
         <div>
-          <Link to="/Beer" className="linkStyle">
-          <p className="menuItem">Beer</p>
-          </Link>
+          <NavLink to="/Beer" className="linkStyle">
+            <p className="menuItem">Beer</p>
+          </NavLink>
         </div>
       </Nav>
 
       <Nav className="menuItemStyle">
-        <Link to="/">
-        <img src={corkbarrellogo} style={imgStyle}></img>
-        </Link>
+        <NavLink to="/">
+          <img src={corkbarrellogo} style={imgStyle}></img>
+        </NavLink>
       </Nav>
 
       <Nav className="menuItemStyle">
         <div>
-          <Link to="/Spirits" className="linkStyle">
-          <p className="menuItem">Spirits</p>
-          </Link>
+          <NavLink to="/Spirits" className="linkStyle">
+            <p className="menuItem">Spirits</p>
+          </NavLink>
         </div>
       </Nav>
       <Nav className="menuItemStyle">
-      <NavDropdown style={navDropStyle} title="More" id="basic-nav-dropdown">
-        <NavDropdown.Item href="/About">About</NavDropdown.Item>
-        <NavDropdown.Item href="/Contact">Contact</NavDropdown.Item>
-      </NavDropdown>
+        <NavDropdown style={navDropStyle} title="More" id="basic-nav-dropdown">
+          <NavDropdown.Item href="/About">About</NavDropdown.Item>
+          <NavDropdown.Item href="/Contact">Contact</NavDropdown.Item>
+        </NavDropdown>
       </Nav>
     </Navbar>
   );
-}
+};
 
 export default NavigationBar;
