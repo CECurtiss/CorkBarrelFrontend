3378c2c74dceae43fcbddef02f58b41d69ca7226
CECurtiss <curtissclinton@gmail.com>
2024-03-08 22:51:22 -0600

    imported image to split pictures on home page. Added reserve wall picture to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import storefront from "../images/storefront.jpg";
+import reservewall from "../images/reservewall.jpg";
+import backgroundslash from "../images/backgroundslash.png";
 
 const Home = () => {
-    const storefrontStyle = {
+    const backgroundImageStyle = {
         width: "100%",
         height: "auto",
         margin: "0",
     }
     return (
         <>
-        <img style={storefrontStyle} src={storefront} alt="storefront" />
+        <img style={backgroundImageStyle} src={storefront} alt="storefront" />
+        <div style={{backgroundColor: "rgb(120, 175, 58)", height: "10px"}}></div>
+        <img src={backgroundslash} alt="backgroundslash" style={{width: "100%", height: "auto", margin: "0"}} />
+        <img style={backgroundImageStyle} src={reservewall} alt="reservewall" />
         <h1>Home Page</h1>
         </>
     )
