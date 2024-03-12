86649c65e68519c4f4e39f730cd1675f21a8eeec
CECurtiss <curtissclinton@gmail.com>
2024-03-11 21:59:05 -0500

    adjusted css and added text to wine section of home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,14 +9,15 @@ const Home = () => {
   return (
     <>
       <img className="storefrontImg" src={storefront} alt="storefront" />
-      <div className="col-12 wineDiv">
+      <div className="col-12 m-2 wineDiv">
         <img
           className="col-6 wineAisles"
           src={wineAisles}
           alt="wine aisles"
         />
-        <div className="wineText">
-          <p>Here, I will write some things about wine.</p>
+        <div className="col-6 wineText">
+          <h1 className="align-top">Wine</h1>
+          <p>Let the knowledgeable staff at Cork & Barrel Wine and Spirits help you navigate our selection of over 2,200 different wines. Visit us today and take advantage of our amazing service and competitive prices. Wether you're buying your go-to wine, or looking for something completely new, we've got a wine for you.</p>
         </div>
       </div>
       <h1>Home Page</h1>
