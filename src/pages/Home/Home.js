d5eb8144cb9308d53a610db6a3fb148cb4ac22cd
CECurtiss <curtissclinton@gmail.com>
2024-03-11 21:34:42 -0500

    renamed img to wineasiles, changed wine image on home page, changed necessary css file name and imported

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,15 +2,22 @@ import React from "react";
 
 import "./Home.css";
 import storefront from "../../images/storefront.jpg";
-import reservewall from "../../images/reservewall.jpg";
+import wineAisles from "../../images/WineAisles.jpg";
+// import reservewall from "../../images/reservewall.jpg";
 
 const Home = () => {
-
   return (
     <>
       <img className="storefrontImg" src={storefront} alt="storefront" />
-      <div>
-        <img className="reservewallImg" src={reservewall} alt="reservewall" />
+      <div className="col-12 wineDiv">
+        <img
+          className="col-6 wineAisles"
+          src={wineAisles}
+          alt="wine aisles"
+        />
+        <div className="wineText">
+          <p>Here, I will write some things about wine.</p>
+        </div>
       </div>
       <h1>Home Page</h1>
     </>
