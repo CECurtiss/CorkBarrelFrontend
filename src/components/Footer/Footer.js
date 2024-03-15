3139c82ce26e2107ab8f54e83916facff28ef83c
CECurtiss <curtissclinton@gmail.com>
2024-03-14 21:56:49 -0500

    added basic footer bar with copilot. Will edit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,32 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <>
-      <p>some footer content</p>
+    <div className="footerContainer">
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <h3>Hours</h3>
+            <p>Monday - Saturday: 9am - 11pm</p>
+            <p>Sunday: 10am - 8pm</p>
+          </div>
+
+          <div className="col-4">
+            <h3>Location</h3>
+            <p>1234 Main St.</p>
+            <p>Anytown, USA 12345</p>
+          </div>
+
+          <div className="col-4">
+            <h3>Contact</h3>
+            <p>Phone: 123-456-7890</p>
+            <p>Email:</p>
+          </div>
+        </div>
+      </div>
+      </div>
     </>
   );
 };
