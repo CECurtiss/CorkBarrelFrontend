0f6847be41aed441ae7610ddd80b9411a2ad39a1
CECurtiss <curtissclinton@gmail.com>
2024-03-14 23:26:15 -0500

    added facebook and instagram icons and links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import facebookIcon from "../../images/icons/fbicon.png";
+import instagramIcon from "../../images/icons/instaicon.png";
 import "./Footer.css";
+
+
 
 const Footer = () => {
   return (
@@ -7,22 +13,37 @@ const Footer = () => {
     <div className="footerContainer">
       <div className="container">
         <div className="row">
-          <div className="col-4">
+          <div className="col-3">
             <h3>Hours</h3>
             <p>Monday - Saturday: 9am - 11pm</p>
             <p>Sunday: 10am - 8pm</p>
           </div>
 
-          <div className="col-4">
-            <h3>Location</h3>
-            <p>1234 Main St.</p>
-            <p>Anytown, USA 12345</p>
+          <div className="col-3">
+            <h3>Locations</h3>
+            <p>1801 West 23rd St</p>
+            <p>901 Mississippi</p>
           </div>
 
-          <div className="col-4">
+          <div className="col-3">
             <h3>Contact</h3>
             <p>Phone: 123-456-7890</p>
             <p>Email:</p>
+          </div>
+
+          <div className="iconLinks col-3">
+            <h3>Follow Us</h3>
+            <Link to="https://www.facebook.com/corkandbarrel" target="_blank">
+              <img className="icon" src={facebookIcon} alt="facebook icon"></img>
+            </Link>
+
+            <Link to="https://www.instagram.com/" target="_blank">
+              <img className="icon" src={instagramIcon} alt="instagram icon"></img>
+            </Link>
+
+            <a href="https://www.twitter.com/">
+              <i class="fab fa-twitter-square"></i>
+            </a>
           </div>
         </div>
       </div>
