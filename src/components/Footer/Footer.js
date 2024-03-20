0e66062b28d30d11bd9a13d1ec90c7684f3c2e1c
CECurtiss <curtissclinton@gmail.com>
2024-03-19 23:26:58 -0500

    added logo to footer and changed some information around

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import footerLogo from "../../images/corkbarrellogo.webp";
 import facebookIcon from "../../images/icons/fbicon.png";
 import instagramIcon from "../../images/icons/instaicon.png";
 import xIcon from "../../images/icons/xicon7.png";
@@ -11,59 +12,69 @@ const Footer = () => {
     <>
       <div className="footerContainer">
         <div className="container">
-          <div className="row">
-            <div className="col-3">
-              <h3>Hours</h3>
-              <p>Monday - Saturday: 9am - 11pm</p>
-              <p>Sunday: 10am - 8pm</p>
-            </div>
+          <div className="footerLogoDiv">
+            <img
+              className="footerLogo"
+              src={footerLogo}
+              alt="Cork and Barrel Logo"
+            ></img>
+          </div>
+          <div>
+            <div className="row">
+              <div className="col-4">
+                <h2>Hours</h2>
+                <p>Monday - Saturday: 9am - 11pm</p>
+                <p>Sunday: 10am - 8pm</p>
+              </div>
 
-            <div className="col-3">
-              <h3>Locations</h3>
-              <p><Link
-                to="https://maps.app.goo.gl/VKvrv1nC8pTBN1kY8"
-                target="_blank"
-              >
-                1801 West 23rd St
-              </Link>
-              </p>
-              <p>
-              <Link
-                to="https://maps.app.goo.gl/7zDNGEm7oHT4JGwN8"
-                target="_blank"
-              >
-                901 Mississippi
-              </Link>
-              </p>
-            </div>
+              <div className="col-4">
+                <h2>Locations</h2>
+                <h3>West</h3>
+                <p>
+                  <Link
+                    to="https://maps.app.goo.gl/VKvrv1nC8pTBN1kY8"
+                    target="_blank"
+                  >
+                    1801 West 23rd St
+                  </Link>
+                </p>
+                <p>785-331-4242</p>
+                <p>
+                  <Link
+                    to="https://maps.app.goo.gl/7zDNGEm7oHT4JGwN8"
+                    target="_blank"
+                  >
+                    901 Mississippi
+                  </Link>
+                </p>
+                <p>785-842-4450</p>
+              </div>
 
-            <div className="col-3">
-              <h3>Contact</h3>
-              <p>23rd St Phone: 785-331-4242</p>
-              <p>Downtown Phone: 785-842-4450</p>
-            </div>
+              <div className="iconLinks col-4">
+                <h2>Follow Us</h2>
+                <Link
+                  to="https://www.facebook.com/corkandbarrel"
+                  target="_blank"
+                >
+                  <img
+                    className="icon"
+                    src={facebookIcon}
+                    alt="facebook icon"
+                  ></img>
+                </Link>
 
-            <div className="iconLinks col-3">
-              <h3>Follow Us</h3>
-              <Link to="https://www.facebook.com/corkandbarrel" target="_blank">
-                <img
-                  className="icon"
-                  src={facebookIcon}
-                  alt="facebook icon"
-                ></img>
-              </Link>
+                <Link to="https://www.instagram.com/corkbarrel" target="_blank">
+                  <img
+                    className="icon"
+                    src={instagramIcon}
+                    alt="instagram icon"
+                  ></img>
+                </Link>
 
-              <Link to="https://www.instagram.com/corkbarrel" target="_blank">
-                <img
-                  className="icon"
-                  src={instagramIcon}
-                  alt="instagram icon"
-                ></img>
-              </Link>
-
-              <Link to="https://www.twitter.com/corkbarrel" target="_blank">
-                <img className="icon" src={xIcon} alt="twitter icon"></img>
-              </Link>
+                <Link to="https://www.twitter.com/corkbarrel" target="_blank">
+                  <img className="icon" src={xIcon} alt="twitter icon"></img>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
