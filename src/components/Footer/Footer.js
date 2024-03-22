ad81d9af9053f2e1a2454cdb6a2823f395a1e1ce
CECurtiss <curtissclinton@gmail.com>
2024-03-21 23:15:46 -0500

    changing positioning in footer, changed hyperlinks from address to location h3

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,38 +21,38 @@ const Footer = () => {
           </div>
           <div>
             <div className="row footerInfo">
-              <div className="col-4 footerColumn">
+              <div className="col-4 footerColumnHrs">
                 <h2>Hours</h2>
                 <p>Monday - Saturday: 9am - 11pm</p>
                 <p>Sunday: 10am - 8pm</p>
               </div>
 
-              <div className="col-4 footerColumn">
+              <div className="col-4 footerColumnLoc">
                 <h2>Locations</h2>
-                <h3>West</h3>
-                <p>
+                <h3>
+                  {" "}
                   <Link
                     to="https://maps.app.goo.gl/VKvrv1nC8pTBN1kY8"
                     target="_blank"
                   >
-                    1801 West 23rd St
+                    West
                   </Link>
-                </p>
+                </h3>
+                <p>1801 West 23rd St</p>
                 <p>785-331-4242</p>
-                <h3>Downtown</h3>
-                <p>
+                <h3>
                   <Link
                     to="https://maps.app.goo.gl/7zDNGEm7oHT4JGwN8"
                     target="_blank"
                   >
-                    901 Mississippi
+                    Downtown
                   </Link>
-                </p>
+                </h3>
+                <p>901 Mississippi</p>
                 <p>785-842-4450</p>
               </div>
 
               <div className="iconLinks col-4">
-                <h2>Follow Us</h2>
                 <Link
                   to="https://www.facebook.com/corkandbarrel"
                   target="_blank"
