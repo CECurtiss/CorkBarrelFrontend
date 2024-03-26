e853802dc1724e4edd7c8b3c3d72edc7b6245af9
CECurtiss <curtissclinton@gmail.com>
2024-03-25 22:06:36 -0500

    reverted back to form in wine.js, added styling to form.

--- src/pages/Wine/Wine.js
+++ src/pages/Wine/Wine.js
@@ -1,8 +1,5 @@
 import React from "react";
-import VIPForm from "../../components/VIPForm";
-
 import "./Wine.css";
-
 
 const Wine = () => {
   return (
@@ -12,7 +9,27 @@ const Wine = () => {
       </div>
       <div className="col-12 VIPContainer">
         <h2 className="col-6">VIP Wine Club Text</h2>
-        <VIPForm />
+        <form className="col-6 VIPFormStyle">
+          <h2>Get Wine Updates!</h2>
+          <h4>News From Cork & Barrel to your Inbox</h4>
+          <label>
+            {" "}
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <label>
+            {" "}
+            Email:
+            <input type="text" name="email" />
+          </label>
+          <label>
+            {" "}
+            Phone:
+            <input type="text" name="phone" />
+          </label>
+          <button type="submit" value="Submit">Submit
+            </button>
+        </form>
       </div>
     </div>
   );
