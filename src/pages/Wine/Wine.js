fd2e8a6cbd64545ef075037af0857c7c016509c0
CECurtiss <curtissclinton@gmail.com>
2024-03-25 21:40:15 -0500

    added VIPForm component to submit user data to wineclub. Imported to Wine.js. Added VIPContainer div to Wine.js

--- src/pages/Wine/Wine.js
+++ src/pages/Wine/Wine.js
@@ -1,11 +1,18 @@
 import React from "react";
+import VIPForm from "../../components/VIPForm";
+
 import "./Wine.css";
+
 
 const Wine = () => {
   return (
     <div>
       <div className="wineHomeImg">
         <h1>Wine</h1>
+      </div>
+      <div className="col-12 VIPContainer">
+        <h2 className="col-6">VIP Wine Club Text</h2>
+        <VIPForm />
       </div>
     </div>
   );
