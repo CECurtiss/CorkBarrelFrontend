5946ac5a25b3a8dd3d485e2c4e5ec45d5ac5f537
CECurtiss <curtissclinton@gmail.com>
2024-03-25 23:08:17 -0500

    added checkboxes to form in wine.js

--- src/pages/Wine/Wine.js
+++ src/pages/Wine/Wine.js
@@ -12,23 +12,27 @@ const Wine = () => {
         <form className="col-6 VIPFormStyle">
           <h2>Get Wine Updates!</h2>
           <h4>News From Cork & Barrel to your Inbox</h4>
-          <label>
-            {" "}
-            Name:
-            <input type="text" name="name" />
-          </label>
-          <label>
-            {" "}
-            Email:
-            <input type="text" name="email" />
-          </label>
-          <label>
-            {" "}
-            Phone:
-            <input type="text" name="phone" />
-          </label>
-          <button type="submit" value="Submit">Submit
-            </button>
+          <label> Name:</label>
+          <input type="text" name="name" />
+
+          <label> Email:</label>
+          <input type="text" name="email" />
+
+          <label> Phone:</label>
+          <input type="text" name="phone" />
+
+          <label>Email Lists</label>
+          <label for="GeneralInterest">
+          <input type="checkbox" id="GeneralInterest" name="General Interest" />
+          General Interest</label>
+
+          <label for="VIPWineClub">
+          <input type="checkbox" id="VIPWineClub" name="VIP Wine Club" />
+          VIP Wine Club</label>
+
+          <button type="submit" value="Submit">
+            Submit
+          </button>
         </form>
       </div>
     </div>
