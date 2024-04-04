eadd20cc6083262e3ac41e3a912ceace466049cf
CECurtiss <curtissclinton@gmail.com>
2024-04-03 23:40:08 -0500

    added spirits intro text and scotch aisle img in spirits.js

--- src/pages/Spirits/Spirits.js
+++ src/pages/Spirits/Spirits.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./Spirits.css";
 
-const Spirits = () => {
-    return (
-        <div>
-        <div className="spiritsHomeImg">
-            <h1>Spirits</h1>
-        </div>
-        </div>
-    );
-    }
+import scotchaisle from "../../images/scotchaisle.jpg";
 
-    export default Spirits;
+const Spirits = () => {
+  return (
+    <div>
+      <div className="spiritsHomeImg">
+        <h1>Spirits</h1>
+      </div>
+
+        <div className="col-12 spiritsContainer">
+            <h2 className="col-6 spiritsPageIntroText">
+                At Cork & Barrel, our selection of spirits is vast and varied. We
+                carry everything from the most popular brands to the most obscure.
+                Our staff is always available to help you find the perfect spirit
+                for your needs. We also carry a wide variety of mixers and garnishes
+                to help you craft the perfect cocktail. Beyond our already low
+                everyday prices, we offer case DISCOUNTS as well.
+            </h2>
+            <img className="col-6 scotchAisleImg" src={scotchaisle} alt="scotch aisle"></img>
+            </div>
+    </div>
+  );
+};
+
+export default Spirits;
