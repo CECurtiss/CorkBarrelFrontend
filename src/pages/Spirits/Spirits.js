1c1e8b9487a6b89eb6fd4363bcd4338e0ce5f8ca
CECurtiss <curtissclinton@gmail.com>
2024-04-04 23:44:34 -0500

    added reserve wall section to spirits.js

--- src/pages/Spirits/Spirits.js
+++ src/pages/Spirits/Spirits.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Spirits.css";
 
 import scotchaisle from "../../images/scotchaisle.jpg";
+import bourbonaisle from "../../images/bourbonaisle.jpg";
 
 const Spirits = () => {
   return (
@@ -10,17 +11,35 @@ const Spirits = () => {
         <h1>Spirits</h1>
       </div>
 
-        <div className="col-12 spiritsContainer">
-            <h2 className="col-6 spiritsPageIntroText">
-                At Cork & Barrel, our selection of spirits is vast and varied. We
-                carry everything from the most popular brands to the most obscure.
-                Our staff is always available to help you find the perfect spirit
-                for your needs. We also carry a wide variety of mixers and garnishes
-                to help you craft the perfect cocktail. Beyond our already low
-                everyday prices, we offer case DISCOUNTS as well.
-            </h2>
-            <img className="col-6 scotchAisleImg" src={scotchaisle} alt="scotch aisle"></img>
-            </div>
+      <div className="col-12 spiritsContainer">
+        <h2 className="col-6 spiritsPageIntroText">
+          At Cork & Barrel, our selection of spirits is vast and varied. We
+          carry everything from the most popular brands to the most obscure. Our
+          staff is always available to help you find the perfect spirit for your
+          needs. We also carry a wide variety of mixers and garnishes to help
+          you craft the perfect cocktail. Beyond our already low everyday
+          prices, we offer case DISCOUNTS as well.
+        </h2>
+        <img
+          className="col-6 scotchAisleImg"
+          src={scotchaisle}
+          alt="scotch aisle"
+        ></img>
+      </div>
+
+      <div className="highEndSpirits">
+        <p>High End Selections</p>
+        <h1 className="reserveSpiritsHeading">Top Shelf Spirits</h1>
+        <div className="reserveSpiritsImgDiv">
+          <h3 className="reserveSpiritsText">some text</h3>
+        </div>
+
+        <img
+          className="reserveSpiritsImg"
+          src={bourbonaisle}
+          alt="Reserve Spirits"
+        />
+      </div>
     </div>
   );
 };
