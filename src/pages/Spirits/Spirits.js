b800c1281b63972d066cbf8bb380d1c32e080492
CECurtiss <curtissclinton@gmail.com>
2024-04-08 18:33:11 -0500

    reformatted high end spirits section in spirits.js. Edited text.

--- src/pages/Spirits/Spirits.js
+++ src/pages/Spirits/Spirits.js
@@ -31,14 +31,19 @@ const Spirits = () => {
         <p>High End Selections</p>
         <h1 className="reserveSpiritsHeading">Top Shelf Spirits</h1>
         <div className="reserveSpiritsImgDiv">
-          <h3 className="reserveSpiritsText">some text</h3>
-        </div>
+          <h3 className="reserveSpiritsText">
+            Hard to find tequila, bourbon, scotch, and more! These brands are
+            high quality and limited in production. Come find those rare spirits
+            for yourself, or as a gift for that special someone who is hard to
+            shop for.
+          </h3>
 
-        <img
-          className="reserveSpiritsImg"
-          src={bourbonaisle}
-          alt="Reserve Spirits"
-        />
+          <img
+            className="reserveSpiritsImg"
+            src={bourbonaisle}
+            alt="Reserve Spirits"
+          />
+        </div>
       </div>
     </div>
   );
