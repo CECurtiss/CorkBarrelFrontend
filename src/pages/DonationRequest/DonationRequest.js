1517c7f85858952acbfaf268be154b4d8f1f56e9
CECurtiss <curtissclinton@gmail.com>
2024-04-12 23:00:11 -0500

    added required fields, inputs, and button to donationrequest.js

--- src/pages/DonationRequest/DonationRequest.js
+++ src/pages/DonationRequest/DonationRequest.js
@@ -1,9 +1,55 @@
 import React from "react";
+import "./DonationRequest.css";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const DonationRequest = () => {
   return (
-    <div>
+    <div className="donationContainer">
       <h1>Donation Request Page</h1>
+      <Form>
+        <Form.Group as={Col} className="mb-5" controlId="formBasicFirstName">
+          <Form.Label>Contact Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Contact Name" />
+        </Form.Group>
+
+        <Form.Group as={Col} className="mb-5" controlId="formBasicLastName">
+          <Form.Label>Contact Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter Contact Email" />
+        </Form.Group>
+
+        <Form.Group as={Col} className="mb-5" controlId="formBasicLastName">
+          <Form.Label>Contact Phone</Form.Label>
+          <Form.Control type="text" placeholder="Enter Contact Phone" />
+        </Form.Group>
+        
+        <Form.Group as={Col} className="mb-5" controlId="formBasicLastName">
+          <Form.Label>Date of Event</Form.Label>
+          <Form.Control type="date" />
+        </Form.Group>
+
+        <Form.Group as={Col} className="mb-5" controlId="formBasicLastName">
+          <Form.Label>Organization Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Organization Name" />
+        </Form.Group>
+
+        <Form.Group as={Col} className="mb-5" controlId="formBasicLastName">
+          <Form.Label>Reason for Request</Form.Label>
+          <Form.Control type="text" placeholder="Enter Request Information"/>
+        </Form.Group>
+
+        <Form.Group as={Col} className="mb-5" controlId="formBasicLastName">
+          <Form.Label>Donation Requested</Form.Label>
+          <Form.Control type="text" placeholder="Enter Requested Donation"/>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit Request
+        </Button>
+      </Form>
     </div>
   );
 };
